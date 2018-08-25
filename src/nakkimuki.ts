7b57a6dafac81648aece2f4abfb032393d8360e2
Olavi Haapala <ojhaapala@gmail.com>
2018-08-25 12:15:57 +0300

    Initial setup and trying out something that totally does not work

---
+++ src/nakkimuki.ts
@@ -0,0 +1,26 @@
+console.log('Nakkimuki started!');
+
+// https://inbox.google.com/ compose -> send button selector
+const inboxSendBtn = document.querySelector('[jsaction="compose2.send"]');
+const inboxToReceivers = document.querySelectorAll(
+  '[jsaction^="mousedown:compose"][jsaction*="chip_mousedown"]'
+);
+
+console.log({ inboxSendBtn, inboxToReceivers });
+if (inboxSendBtn) {
+  inboxSendBtn.addEventListener('click', (e) => {
+    console.log(e, 'Clickedy click');
+    inboxToReceivers.forEach((receiver) =>
+      console.log({ receiver }, receiver['email'])
+    );
+  });
+}
+
+// Does not work because GMAIL is doing ugly stuff
+document.addEventListener('click', (e) => {
+  console.log(e.target, 'event captured');
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+document.body.style.border = '5px solid red';
